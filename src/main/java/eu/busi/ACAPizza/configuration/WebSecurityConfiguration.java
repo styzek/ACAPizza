0e72e3857bb2c7de7ac34caf7ec1eba9b6b9ca2d
Abdellah <elachhab.abdellah@gmail.com>
2018-11-09 10:06:06 +0100

    avant panier

--- src/main/java/eu/busi/ACAPizza/configuration/WebSecurityConfiguration.java
+++ src/main/java/eu/busi/ACAPizza/configuration/WebSecurityConfiguration.java
@@ -1,10 +1,7 @@
 package eu.busi.ACAPizza.configuration;
 
-import eu.busi.ACAPizza.dataAccess.dao.UserDAO;
-import eu.busi.ACAPizza.service.UserDetailsServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,14 +11,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 //    private static final String LOGIN_REQUEST ="/login";
-    private static final String[] AUTHORIZED_REQUEST_ANYBODY = new String []{"/home", "/pizza", "/inscription", "/inscription/send"};
+    private static final String[] AUTHORIZED_REQUEST_ANYBODY = new String []{"/panier", "/home", "/pizza", "/inscription", "/inscription/send"};
     private static final String[] AUTHORIZED_REQUEST_ADMIN = new String []{"/admin"};
     private static final String[] AUTHORIZED_REQUEST_USER = new String []{"/login"};
 
---
+++ src/main/java/eu/busi/ACAPizza/model/Panier.java
@@ -0,0 +1,5 @@
+package eu.busi.ACAPizza.model;
+
+public class Panier {
+
+}
